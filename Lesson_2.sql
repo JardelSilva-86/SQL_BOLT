d9723e00e521e756000a7d10d0ddb152468925af
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2023-02-27 16:32:01 -0300

    Update Lesson_2.sql

--- Lesson_2.sql
+++ Lesson_2.sql
@@ -12,3 +12,27 @@ WHERE id = 6;
 /*
 2. Find the movies released in the year between 2000 and 2010
 */
+
+SELECT
+  title
+FROM movies
+WHERE year BETWEEN 2000 AND 2010;
+
+/*
+3. Find the movies not released ind year between 2000 and 2010.
+*/
+
+SELECT
+  title
+FROM movies
+WHERE year NOT BETWEEN 2000 AND 2010;
+
+/*
+4. Find the first 5 Pixar movies and their release year
+*/
+
+SELECT 
+    title,
+    year
+FROM movies
+WHERE year <= 2003;
