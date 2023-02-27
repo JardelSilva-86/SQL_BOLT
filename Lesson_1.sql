fe098379648a6aad7dcf4cb9f765dc2ec1a67d96
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2023-02-27 16:09:26 -0300

    Create Lesson_1.sql
    All the exercises from lesson 1

---
+++ Lesson_1.sql
@@ -0,0 +1,39 @@
+/*
+1. Find the title of each film.
+*/
+
+SELECT Title
+FROM movies;
+
+/*
+2. Find the director of each film
+*/
+
+SELECT Director
+FROM Movies;
+
+/*
+3. Find title an director of each film
+*/
+
+SELECT
+  Title,
+  Director
+FROM movies;
+
+/*
+4. Find title an year of each film
+*/
+
+SELECT
+  title,
+  year
+FROM
+  movies;
+  
+/*
+5. Find all the information about each film
+*/
+
+SELECT *
+FROM movies;
