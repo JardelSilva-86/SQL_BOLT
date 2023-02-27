283cb196b947e9b249f812eba02be2b4246dc73f
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2023-02-27 16:22:15 -0300

    Update Lesson_2.sql

--- Lesson_2.sql
+++ Lesson_2.sql
@@ -1,2 +1,14 @@
-/* Lesson 2 - Queries with constraints (Pt.1) */
+/* Lesson 2: Queries with constraints (Pt.1) */
 
+/*
+1. Find the movies with a row id of 6
+*/
+
+SELECT
+  title
+FROM movies
+WHERE id = 6;
+
+/*
+2. Find the movies released in the year between 2000 and 2010
+*/
