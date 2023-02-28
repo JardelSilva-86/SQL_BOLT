e34a19e360510e27ade5da66891ad60725b126ec
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2023-02-28 09:58:28 -0300

    Create Lesson_Review.sql

---
+++ Lesson_Review.sql
@@ -0,0 +1,25 @@
+/*SQL Review: Simple SELECT Queries */
+
+/*
+1. List all the Canadian cities and their populations
+*/
+
+
+/*
+2. Order all the cities in the United States by their latitude from north so south
+*/
+
+
+/*
+3. List all the cities west of Chicago, ordered from west to east.
+*/
+
+
+/*
+4. List the two largest cities in Mexico (by population)
+*/
+
+
+/*
+5. List the third anf fourth largest cities (by population) in the Unites States and thier population.
+*/
