c83be37e7f5b1db43e89363d511198c31c83f067
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2023-03-02 08:34:19 -0300

    Update Lesson_6.sql

--- Lesson_6.sql
+++ Lesson_6.sql
@@ -1,1 +1,14 @@
 /*Lesson 6: Multi-table queries with joins */
+
+/*
+1. Find the domestic and international sales for each movie.
+*/
+
+/*
+2. Show the sales numbers for each movie that did better internationally tarher than domestically.
+*/
+
+/*
+3. List all the movies by their ratings in descending order.
+*/
+
