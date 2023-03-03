e4bc9f69d952fd78cf03349de50d871dea508cce
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2023-03-03 07:02:42 -0300

    Update Lesson_7.sql

--- Lesson_7.sql
+++ Lesson_7.sql
@@ -1,1 +1,13 @@
 /*Lesson 7: Outer Joins */
+
+/*
+1. Find the list of all buildings that have employees.
+*/
+
+/*
+2. Find the list of all buildings and their capacity.
+*/
+
+/*
+3. List all buildings and the distinct employee roles in each building (including empty buildings).
+*/
