6ce9db21e46f740831af40969b3c0ac9b9b01aff
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2023-03-02 21:24:44 -0300

    Update Lesson_6.sql

--- Lesson_6.sql
+++ Lesson_6.sql
@@ -4,12 +4,32 @@
 1. Find the domestic and international sales for each movie.
 */
 SELECT 
+  T1.Title,
+  T2.Domestic_sales,
+  T2.International_sales
+FROM movies T1
+INNER JOIN Boxoffice T2
+  ON T1.id = T2.Movie_id;
 
 /*
 2. Show the sales numbers for each movie that did better internationally tarher than domestically.
 */
+SELECT 
+  T1.Title,
+  T2.Domestic_sales,
+  T2.International_sales
+FROM movies T1
+INNER JOIN Boxoffice T2
+  ON T1.id = T2.Movie_id
+WHERE T2.International_Sales > Domestic_Sales;;
 
 /*
 3. List all the movies by their ratings in descending order.
 */
-
+SELECT 
+  T1.Title,
+  T2.Rating
+FROM movies T1
+INNER JOIN Boxoffice T2
+  ON T1.id = T2.Movie_id
+ORDER BY Rating DESC;
